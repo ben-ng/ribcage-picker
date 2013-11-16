d799c23b5e4c2e4d534b59a98d0cf82ab99cd096
Ben Ng <me@benng.me>
2013-11-16 04:17:32 -0600

    Add LICENSE

--- picker.js
+++ picker.js
@@ -1,14 +1,5 @@
 /**
- *
- * Find more about the Spinning Wheel function at
- * http://cubiq.org/spinning-wheel-on-webkit-for-iphone-ipod-touch/11
- *
- * Copyright (c) 2009 Matteo Spinelli, http://cubiq.org/
- * Released under MIT license
- * http://cubiq.org/dropbox/mit-license.txt
- *
- * Version 1.4 - Last updated: 2009.07.09
- *
+ * Derived from Matteo Spinelli's original implementation, see LICENSE for details
  */
 
 var Ribcage = require('ribcage-view')

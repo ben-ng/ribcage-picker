2bcfc93609767f8193c30fe6c78761d16e265902
Ben Ng <me@benng.me>
2013-12-25 00:29:47 -0800

    Add uglify

--- update.sh
+++ update.sh
@@ -1,2 +1,7 @@
-git co master -- test/build && cp -r test/build/* . && rm -r test
+git co master -- test/build
+cp -r test/build/* .
+rm -r test
+mv bundle.js bundle.dev.js
+uglifyjs bundle.dev.js > bundle.js
+echo "Done"
 
